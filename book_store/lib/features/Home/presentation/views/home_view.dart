57084532378b42aa26110edfa81f9115b4147704
alidiab2000 <53220716+alidiab2000@users.noreply.github.com>
2023-12-08 19:17:13 +0200

    delete drawer

--- book_store/lib/features/Home/presentation/views/home_view.dart
+++ book_store/lib/features/Home/presentation/views/home_view.dart
@@ -1,6 +1,4 @@
-import 'package:book_store/features/Home/presentation/views/widgets/my_drawer.dart';
 import 'package:flutter/material.dart';
-
 import 'widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -10,7 +8,7 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: HomeViewBody(),
-      drawer: MyDrawer(),
+       
     );
   }
 }
